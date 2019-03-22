1b8ab18ad8845ced74150817d1be3f1856627891
Philipp Schwarz <schwarz_philipp@hotmail.de>
2019-03-22 18:54:44 +0100

    added test

--- src/test/JUnitTestFrame.java
+++ src/test/JUnitTestFrame.java
@@ -1,5 +1,19 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import set.*;
+import types.*;
+
 public class JUnitTestFrame {
     final int timelimit = 5_000; // ms
+
+    @Test(timeout = timelimit)
+    public void testAddSetArray() {
+        SetInterface sa = new SetArray();
+        Elem<Integer> e = new Elem(new Integer(5));
+        Pos p = sa.add(e);
+
+        assertEquals(1, sa.size());
+    }
 }
