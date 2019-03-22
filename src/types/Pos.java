1590b98b0d60f9e9e1eb8f826924254a582d52f2
Finn Jannsen <finn-frederik.jannsen@haw-hamburg.de>
2019-03-22 17:28:55 +0100

    pos changed

--- src/types/Pos.java
+++ src/types/Pos.java
@@ -3,9 +3,17 @@ package types;
 
 public class Pos<T> {
 	public Boolean isValid;
-	public T pointer;
+	protected T pointer;
 	
 	public Pos() {
 		this.isValid = false;
+	}
+
+	public T getPointer() {
+		return pointer;
+	}
+
+	public void setPointer(T pointer) {
+		this.pointer = pointer;
 	}
 }
