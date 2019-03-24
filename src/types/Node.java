e334fed5770ab0b96f3ad2032da90e07153e00b2
Philipp Schwarz <schwarz_philipp@hotmail.de>
2019-03-24 20:48:08 +0100

    added javadoc to node

--- src/types/Node.java
+++ src/types/Node.java
@@ -1,49 +1,69 @@
 package types;
 
+/**
+ * A Node that contains an Elem object and two Nodes
+ */
 public class Node {
     private Node next;
     private Node previous;
     private Elem elem;
 
+    /**
+     * A constructor that sets every attribute to null
+     */
     public Node() {
         this.elem = null;
         this.next = null;
         this.previous = null;
     }
 
+    /**
+     * A constructor where you can directly set the elem
+     * @param elem The elem
+     */
     public Node(Elem elem) {
         this.elem = elem;
         this.next = null;
         this.previous = null;
     }
 
-    public Node(Elem elem, Node next, Node previous) {
-        this.elem = elem;
-        this.next = next;
-        this.previous = previous;
-    }
-
+    /**
+     * Returns the next node
+     * @return The node
+     */
     public Node getNext() {
         return next;
     }
 
+    /**
+     * Set the next node
+     * @param next The node
+     */
     public void setNext(Node next) {
         this.next = next;
     }
 
+    /**
+     * Returns the previous node
+     * @return The node
+     */
     public Node getPrevious() {
         return previous;
     }
 
+    /**
+     * Sets the previous node
+     * @param previous The node
+     */
     public void setPrevious(Node previous) {
         this.previous = previous;
     }
 
+    /**
+     * Returns the elem
+     * @return The elem
+     */
     public Elem getElem() {
         return elem;
-    }
-
-    public void setElem(Elem elem) {
-        this.elem = elem;
     }
 }
