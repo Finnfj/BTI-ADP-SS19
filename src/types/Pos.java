e3be83b75ce4c479bc4863f1d236a2f662ec28c1
Finn Jannsen <finn-frederik.jannsen@haw-hamburg.de>
2019-03-24 15:55:30 +0100

    added indexes to pos

--- src/types/Pos.java
+++ src/types/Pos.java
@@ -6,6 +6,8 @@ public class Pos<T> {
 	public Boolean isValid;
 	protected T pointer;
 	protected SetInterface set;
+	private int previousIndex;
+	private int nextIndex; 
 
 	public Pos(T pointer, SetInterface set) {
 		this.isValid = false;
@@ -13,8 +15,24 @@ public class Pos<T> {
 		this.set = set;
 	}
 
+	public Pos(T pointer, SetInterface set, int previousIndex, int nextIndex) {
+		this.isValid = false;
+		this.pointer = pointer;
+		this.set = set;
+		this.previousIndex = previousIndex;
+		this.nextIndex = nextIndex;
+	}
+
 	public T getPointer() {
 		return pointer;
+	}
+
+	public int getPreviousIndex() {
+		return previousIndex;
+	}
+
+	public int getNextIndex() {
+		return nextIndex;
 	}
 
 	public void setPointer(T pointer) {
