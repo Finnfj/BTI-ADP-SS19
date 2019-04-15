2750bd9085f535ea87a29f6a0eb6f79335db9352
Philipp Schwarz <schwarz_philipp@hotmail.de>
2019-04-15 19:04:21 +0200

    made recursive solution more heap efficient

--- A4/src/pascalTriangleCalculator/PascalTriangleCalculatorRecursive.java
+++ A4/src/pascalTriangleCalculator/PascalTriangleCalculatorRecursive.java
@@ -3,12 +3,10 @@ package pascalTriangleCalculator;
 public class PascalTriangleCalculatorRecursive implements PascalTriangleCalculator {
     private long counter;
     private int[][] triangle;
-    private boolean[][] triangleCalced;
 
     public PascalTriangleCalculatorRecursive() {
         counter = 0;
         triangle = null;
-        triangleCalced = null;
     }
 
     @Override
@@ -18,10 +16,8 @@ public class PascalTriangleCalculatorRecursive implements PascalTriangleCalculat
         }
 
         triangle = new int[rowNum][];
-        triangleCalced = new boolean[rowNum][];
         for (int i = 0; i < triangle.length; i++) {
             triangle[i] = new int[i+1];
-            triangleCalced[i] = new boolean[i+1];
         }
 
         int[] row = new int[rowNum];
@@ -39,12 +35,11 @@ public class PascalTriangleCalculatorRecursive implements PascalTriangleCalculat
         if (colNum == 0 || colNum == rowNum - 1) {
             return 1;
         } else {
-            if (triangleCalced[rowNum-1][colNum]) {
+            if (triangle[rowNum-1][colNum] != 0) {
                 return triangle[rowNum-1][colNum];
             } else {
                 int num = calculateNum(rowNum - 1, colNum - 1) + calculateNum(rowNum - 1, colNum);
                 triangle[rowNum-1][colNum] = num;
-                triangleCalced[rowNum-1][colNum] = true;
                 return num;
             }
         }
