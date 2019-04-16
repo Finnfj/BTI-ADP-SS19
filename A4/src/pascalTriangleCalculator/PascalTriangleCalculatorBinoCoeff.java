fdd6934c9418029134bced26e0520215e82794f4
Philipp Schwarz <schwarz_philipp@hotmail.de>
2019-04-16 13:52:57 +0200

    made binocoeff more efficient, doesnt work though

--- A4/src/pascalTriangleCalculator/PascalTriangleCalculatorBinoCoeff.java
+++ A4/src/pascalTriangleCalculator/PascalTriangleCalculatorBinoCoeff.java
@@ -35,10 +35,23 @@ public class PascalTriangleCalculatorBinoCoeff implements PascalTriangleCalculat
         for (int i = 0; i < row.length; i++) {
             counter++;
             // formula: n! / (r! * (n-r)!)
-            long top = getFactorial(rowNum-1);
-            long bottom = (getFactorial(i) * getFactorial(rowNum-1-i));
+            // long top = getFactorial(rowNum-1);
+            // long bottom = (getFactorial(i) * getFactorial(rowNum-1-i));
 
-            row[i] = (int) (top / bottom);
+            if (i == 0 || i == rowNum) {
+                row[i] = 1;
+                // if we are at the second or second to last column, the answer is always the row size - 1
+            } else if (i == 1 || i == rowNum - 1) {
+                row[i] = rowNum - 1;
+            } else {
+                long top = rowNum - 1;
+                for (long j = 1; j <= i; j++) {
+                    top *= rowNum - 1 - j;
+                }
+                long bottom = getFactorial(i);
+
+                row[i] = (int) (top / bottom);
+            }
         }
         return row;
     }
