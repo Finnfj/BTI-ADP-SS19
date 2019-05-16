403edc20e1d23355325adbdea25fff59fb4b3349
Philipp Schwarz <schwarz_philipp@hotmail.de>
2019-05-16 14:10:03 +0200

    changed testr

--- A7/src/test/JUnitTestFrameBSTNodeLinking.java
+++ A7/src/test/JUnitTestFrameBSTNodeLinking.java
@@ -12,10 +12,11 @@ public class JUnitTestFrameBSTNodeLinking {
     private static void testOne() {
         BinarySearchTreeI bstnl = new BinarySerachTreeNodeLinking();
         bstnl.addData(1);
-        bstnl.addData(2);
+        bstnl.addData(3);
         bstnl.addData(1);
         bstnl.addData(0);
-        bstnl.addData(3);
+        bstnl.addData(2);
+        bstnl.addData(4);
         bstnl.printTree(PrintVariant.INORDER);
         bstnl.printTree(PrintVariant.PREORDER);
         bstnl.printTree(PrintVariant.POSTORDER);
