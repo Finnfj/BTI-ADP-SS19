e5bd252e4d354b586e1e1007aeca6585ab9f2eef
Philipp Schwarz <schwarz_philipp@hotmail.de>
2019-05-16 14:18:41 +0200

    added tests

--- A7/src/test/JUnitTestFrameBSTNodeLinking.java
+++ A7/src/test/JUnitTestFrameBSTNodeLinking.java
@@ -6,7 +6,7 @@ import binarySearchTree.PrintVariant;
 
 public class JUnitTestFrameBSTNodeLinking {
     public static void main(String... args) {
-        testOne();
+        testThree();
     }
 
     private static void testOne() {
@@ -17,6 +17,30 @@ public class JUnitTestFrameBSTNodeLinking {
         bstnl.addData(0);
         bstnl.addData(2);
         bstnl.addData(4);
+        bstnl.printTree(PrintVariant.INORDER);
+        bstnl.printTree(PrintVariant.PREORDER);
+        bstnl.printTree(PrintVariant.POSTORDER);
+    }
+
+    private static void testTwo() {
+        BinarySearchTreeI bstnl = new BinarySerachTreeNodeLinking();
+        bstnl.addData("Hallo");
+        bstnl.addData("Welto");
+        bstnl.addData("Hanso");
+        bstnl.addData("Peter");
+        bstnl.addData("Bratw");
+        bstnl.printTree(PrintVariant.INORDER);
+        bstnl.printTree(PrintVariant.PREORDER);
+        bstnl.printTree(PrintVariant.POSTORDER);
+    }
+
+    private static void testThree() {
+        BinarySearchTreeI bstnl = new BinarySerachTreeNodeLinking();
+        bstnl.addData("Hallo");
+        bstnl.addData("Welto");
+        bstnl.addData(12345);
+        bstnl.addData(98765);
+        bstnl.addData("Bratw");
         bstnl.printTree(PrintVariant.INORDER);
         bstnl.printTree(PrintVariant.PREORDER);
         bstnl.printTree(PrintVariant.POSTORDER);
