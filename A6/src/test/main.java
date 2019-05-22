4c56f5fd2b1f909dcb67ecc8edacc4b4e19c7b7c
Philipp Schwarz <schwarz_philipp@hotmail.de>
2019-05-22 16:14:40 +0200

    cleaned

---
+++ A6/src/test/main.java
@@ -0,0 +1,9 @@
+package test;
+
+public class main {
+    public static void main(String... args) {
+        for(int i = 2; i < 202; i+=10) {
+            System.out.println("=SUMME(A" + i + ":A" + (i+9) + ")/10");
+        }
+    }
+}
