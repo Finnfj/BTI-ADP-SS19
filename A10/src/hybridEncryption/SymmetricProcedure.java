b1057dac4637e4088f899932f5a8c2c4bfd266a4
Philipp Schwarz <schwarz_philipp@hotmail.de>
2019-06-02 19:32:24 +0200

    implemented A10.2

--- A10/src/hybridEncryption/SymmetricProcedure.java
+++ A10/src/hybridEncryption/SymmetricProcedure.java
@@ -6,28 +6,54 @@ import java.util.Random;
 
 public class SymmetricProcedure {
     private final int BLOCKSIZE = 16;
+    private final int ROUNDS = 12;
+    private final byte PADDING = 0x20;
 
-    public void encryptMessage(String message) {
+    public byte[] encryptMessage(String message) {
         // put the message as bytes into an array with an offset at the beginning
-        byte[] messageBytesWOffset = new byte[message.length() + BLOCKSIZE];
-        System.arraycopy(message.getBytes(), 0, messageBytesWOffset, BLOCKSIZE, message.length());
+        byte[] messageBytes = message.getBytes();
+
+        // since our array has blocks, we need to add padding in case the message does not fill out the last block completely
+        // find out how much extra space we need
+        int messageBytesOffsetPaddingSize = messageBytes.length + BLOCKSIZE;
+        while(messageBytesOffsetPaddingSize % BLOCKSIZE != 0) {
+            messageBytesOffsetPaddingSize++;
+        }
+
+        // create a new byte array and copy the message bytes into it after the key offset
+        byte[] messageBytesOffsetPadding = new byte[messageBytesOffsetPaddingSize];
+        System.arraycopy(messageBytes, 0, messageBytesOffsetPadding, BLOCKSIZE, messageBytes.length);
+
+        // add the padding
+        for(int i = messageBytes.length + BLOCKSIZE; i < messageBytesOffsetPaddingSize; i++) {
+            messageBytesOffsetPadding[i] = PADDING;
+        }
 
         // create a random session key by filling a byte array with half the blocksize as length and the put it into the beginning of the byte array with the message
         Random rnd = new SecureRandom();
         byte[] sessionKey = new byte[BLOCKSIZE/2];
         rnd.nextBytes(sessionKey);
         for(int i = 0; i < BLOCKSIZE/2; i++) {
-            messageBytesWOffset[i] = sessionKey[i];
+            messageBytesOffsetPadding[i] = sessionKey[i];
         }
 
-        BigInteger k = new BigInteger(64, new SecureRandom());
-        BigInteger l = BigInteger.ZERO;
-        BigInteger r = BigInteger.ZERO;
-
-        for(int i = 0; i < 12; i++) {
-            l = r;
-            //r =
+        // extract the left and right part of a block into arrays and swap them ROUND times with the feistelblock
+        final int amountOfMessageBlocks = (messageBytesOffsetPaddingSize-BLOCKSIZE)/BLOCKSIZE;
+        for(int i = 0; i < amountOfMessageBlocks; i++) {
+            byte[] left = new byte[BLOCKSIZE/2];
+            byte[] right = new byte[BLOCKSIZE/2];
+            System.arraycopy(messageBytesOffsetPadding, i*BLOCKSIZE, left, 0, left.length);
+            System.arraycopy(messageBytesOffsetPadding, i*BLOCKSIZE+BLOCKSIZE/2, right, 0, right.length);
+            Feistelblock fb = new Feistelblock(left, right, sessionKey, BLOCKSIZE);
+            for (int j = 0; j < ROUNDS; j++) {
+                fb.swap();
+            }
+            // copy the feistel swapped arrays back into the main array
+            System.arraycopy(fb.getLeft(), 0, messageBytesOffsetPadding, i*BLOCKSIZE+BLOCKSIZE, left.length);
+            System.arraycopy(fb.getRight(), 0, messageBytesOffsetPadding, i*BLOCKSIZE+BLOCKSIZE/2+BLOCKSIZE, right.length);
         }
+
+        return messageBytesOffsetPadding;
     }
 
 }
--- A10/src/test/TestFrame.java
+++ A10/src/test/TestFrame.java
@@ -1,14 +1,16 @@
 package test;
 
 import hybridEncryption.RSAKeys;
+import hybridEncryption.SymmetricProcedure;
 import hybridEncryption.UnsymmetricProcedure;
 
 import java.math.BigInteger;
 
 public class TestFrame {
     public static void main(String... args) {
-        testRSAMessageEncryption();
-        testRSAKeysNegative();
+        //testRSAMessageEncryption();
+        //testRSAKeysNegative();
+        testFeistelEncryption();
     }
 
     public static void testRSAMessageEncryption() {
@@ -16,17 +18,35 @@ public class TestFrame {
 
         BigInteger[] encryptedMessage = rsaKeys.encryptMessage("Hello");
         String decrpytedMessage = rsaKeys.decryptMessage(encryptedMessage);
+        System.out.println(decrpytedMessage);
     }
 
     public static void testRSAKeysNegative() {
+        boolean pubkeyNegative = false;
+        boolean privKeyNegative = false;
         for(int i = 0; i < 1000; i++) {
             RSAKeys keys = UnsymmetricProcedure.generateKeys(64);
             if(keys.getPublicKey().compareTo(BigInteger.ZERO) < 0) {
-                System.out.println("pubKey is negative");
+                pubkeyNegative = true;
             }
             if(keys.getPrivateKey().compareTo(BigInteger.ZERO) < 0) {
-                System.out.println("pubKey is negative");
+                privKeyNegative = true;
             }
         }
+        if (pubkeyNegative) {
+            System.out.println("pubkey was atleast once negative");
+        }
+        if (privKeyNegative) {
+            System.out.println("privatekey was atleast once negative");
+        }
+        if (!pubkeyNegative && !privKeyNegative) {
+            System.out.println("Both Keys are fine");
+        }
+    }
+
+    public static void testFeistelEncryption() {
+        SymmetricProcedure sm = new SymmetricProcedure();
+        byte[] result = sm.encryptMessage("Hallo");
+        System.out.println(new String(result));
     }
 }
