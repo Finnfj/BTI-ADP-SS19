592b7807d6387281e6b3ecff3ded1ff56f0fb02e
Finn Jannsen <finnjannsen@yahoo.de>
2019-06-02 15:28:07 +0200

    doc updated

--- A8/src/test/JUnitTestFrameBSTNodeLinking.java
+++ A8/src/test/JUnitTestFrameBSTNodeLinking.java
@@ -37,22 +37,32 @@ public class JUnitTestFrameBSTNodeLinking {
     }
     
     private static void testTwo() {
-        for (int i = 10; i <= 1_000_000; i *= 10) {
-            BinarySearchTreeI bstnl = new BinarySearchTreeNodeLinking();
-            long testSum = 0;
-            List<Integer> keymap = IntStream.range(0, i).boxed().collect(Collectors.toCollection(ArrayList<Integer>::new));
-            Collections.shuffle(keymap);
-            System.out.println("List size: " + i + "-------------");
-            for (int key : keymap) {
-                bstnl.addData(key);
-                testSum += key;
-            }
-            long startTime = System.currentTimeMillis();
-            bstnl.updateSum();
-            long sum = bstnl.getSum(0, 10 * i);
-            long endTime = System.currentTimeMillis();
-            long time = endTime - startTime;
-            System.out.println("testSum = " + testSum + "\nsum = " + sum + "\ntime = " + time + " ms");
-        }
+    	long timeA[] = {0,0,0,0,0,0,0};
+    	for (int j = 0; j < 10; j++) {
+    		int z = 0;
+	        for (int i = 10; i <= 1_000_0000; i *= 10) {
+	            BinarySearchTreeI bstnl = new BinarySearchTreeNodeLinking();
+	            long testSum = 0;
+	            List<Integer> keymap = IntStream.range(0, i).boxed().collect(Collectors.toCollection(ArrayList<Integer>::new));
+	            Collections.shuffle(keymap);
+	            System.out.println("List size: " + i + "-------------");
+	            for (int key : keymap) {
+	                bstnl.addData(key);
+	                testSum += key;
+	            }
+	            long startTime = System.currentTimeMillis();
+	            bstnl.updateSum();
+	            long sum = bstnl.getSum(0, 10 * i);
+	            long endTime = System.currentTimeMillis();
+	            long time = endTime - startTime;
+	            System.out.println("testSum = " + testSum + "\nsum = " + sum + "\ntime = " + time + " ms");
+	            timeA[z] += time;
+	            z++;
+	        }
+    	}
+    	for (int j = 0; j < 7; j++) {
+    		timeA[j] = timeA[j] / 10;
+        	System.out.println(timeA[j] + "ms");
+    	}
     }
 }
